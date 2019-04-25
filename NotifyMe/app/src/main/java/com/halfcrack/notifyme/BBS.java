deeb0b9bad2390b75284bbb3e5dee015aa63ef62
Alina kansakar <kansakaralina@gmail.com>
2019-04-25 23:31:02 +0545

    yeti ho haiigit add .

---
+++ NotifyMe/app/src/main/java/com/halfcrack/notifyme/BBS.java
@@ -0,0 +1,13 @@
+package com.halfcrack.notifyme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BBS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bbs);
+    }
+}
